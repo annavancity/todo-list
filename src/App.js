3ae7e2d30b9b45e196a87268dc595f65331630df
foxstudent103194 <anna.designers@yahoo.com>
2023-01-03 09:36:04 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Todo } from './Todo';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <h1>What's on your Todo List?</h1>
+      </div>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,66 @@
+import { Component } from "react";
+
+export class Todo extends Component {
+    state = {
+        userInput: " ",
+        todoList: []
+    }
+
+    onChangeEvent = (e) => {
+        this.setState({userInput: e})
+        console.log(e)
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    addItem = (input) => {
+        if (input === "") {
+            alert("Please enter what's on your agenda")
+        }
+        else {
+            let listArray = this.state.todoList;
+            listArray.unshift(input);
+            this.setState({todoList: listArray, userInput: " "})
+        }
+    }
+
+    crossedWord = (event) => {
+        const li = event.target;
+        li.classList.toggle('crossed');
+    }
+
+    deleteItems = () => {
+        let listArray = this.state.todoList;
+        listArray = [];
+        this.setState({todoList: listArray})
+    }
+
+    render() {
+        return (
+        <div className="container">
+            <form onSubmit={this.onFormSubmit}>
+                <div className="input-container">
+                    <input type="text" placeholder="e.g. study" onChange={(e) =>{this.onChangeEvent(e.target.value)}} value={this.state.userInput} />
+                    <button onClick={() => this.addItem(this.state.userInput)} className="add" >➕</button>
+                </div>
+                <div>
+                    <ul>
+                        {this.state.todoList.map((item,index) => (
+                            <li onClick={this.crossedWord} key={index} className="input-container">
+                                <button className="edit btn">Edit</button>
+                                <button className="delete-item btn">Delete</button>
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="container">
+                    <button onClick={() => this.deleteItems()} className="delete">Clear All</button>
+                </div>
+            </form>
+        </div>
+        )
+    }
+}
